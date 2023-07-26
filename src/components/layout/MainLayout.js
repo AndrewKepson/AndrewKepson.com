e75af92091ff0d86f2c452b6fbbd94cf41dda360
RedRocksWebDevelopment <websites@redrockswebdevelopment.com>
2023-07-25 19:21:04 -0600

    Regular updates. Added page transition effect

--- src/components/layout/MainLayout.js
+++ src/components/layout/MainLayout.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Slice } from 'gatsby'
 
+import { PageFadeIn } from '../Animations'
+
 export const Layout = ({ children }) => (
   <div className="m-0 h-screen w-full overflow-x-hidden bg-white p-0">
     <Slice alias="header" />
+    <PageFadeIn>
     <div className="flex min-h-full flex-col px-4 text-gray-500 lg:px-16 lg:py-6">
       <main className="h-100 flex justify-center pt-4 lg:pt-8">{children}</main>
-    </div>
+    </div></PageFadeIn>
     <Slice alias="footer" />
   </div>
 )
