1f2dbdc6fe680ea6c81bcb5c3ec1dbef7a3d1af9
RedRocksWebDevelopment <websites@redrockswebdevelopment.com>
2024-09-10 02:15:16 -0600

    setup post templates

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # AndrewKepson.com
+
+This is my personal website. It is currently built with Astro and headless WordPress, styled with Tailwind CSS.
+
+Always expecting to spend my career doing some form of writing, I studied English literature in college. Shortly after I learned about content writing and blogging, which seemed like the best way to make money with writing. I quickly became interested in SEO, and began working in the digital marketing industry. I had built some websites and learned web development basics as a child, but interacting with them daily reignited my interest, and I knew I wanted to be building websites, not just optimizing them.
+
+In 2020 I had an opportunity to take part in the full stack coding bootcamp at University of Denver. It was exciting to finally learn how the content I wrote in a Content Management System made it to the front end of a website. The bootcamp was focused on MERN stack web development, and I was attracted to React because of its potential regarding site performance, including Core Web Vitals. My interests in SEO and building websites were merging closer together, and I loved figuring out how to implement technical SEO in the context of a modern website.
+
+As my career progressed I moved into Frontend Development. Now I work as a Software Engineer focused on React, Astro, headless CMS development, and automation.
+
+This website is built with Astro and manages content with headless WordPress. On this blog I enjoy writing about topics in headless WordPress, SEO, Astro development, and automation. I also use it to share portfolio projects in web development. Feel free to get in touch with me using the contact page.
--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,25 +1,100 @@
+import * as cheerio from "cheerio";
+
 // * Utility Functions
-export function sortArrayObjectsByProperty(arr, property) {
-	if (!arr || !arr.every((element) => element.hasOwnProperty(property))) {
-		throw new Error(`Every element must have the property '${property}'`);
+export const chooseCategoryColor = (category) => {
+	let color;
+
+	switch (category) {
+		case "Headless Wordpress":
+			color = "bg-purple-200 text-purple-600";
+			break;
+		case "Blockchain":
+			color = "bg-gray-200 text-gray-600";
+			break;
+		case "Digital Marketing":
+			color = "bg-emerald-200 text-emerald-600";
+			break;
+		case "SEO":
+			color = "bg-red-200 text-red-600";
+			break;
+		case "Privacy First Marketing":
+			color = "bg-orange-200 text-orange-700";
+			break;
+		default:
+			color = "bg-purple-200 text-purple-600";
 	}
 
-	return arr.sort((a, b) => {
-		let propA = a[property];
-		let propB = b[property];
+	return color;
+};
 
-		return propA.localeCompare(propB);
-	});
-}
+export const chooseTagColor = (tag) => {
+	let color;
 
-export function sortEdgesByTitle(edges) {
-	if (!edges || !edges.every((edge) => edge.node && edge.node.title)) {
-		throw new Error("Each edge must have a node with a title");
+	switch (tag) {
+		case "Web Development":
+			color = "bg-red-300";
+			break;
+		case "React":
+			color = "bg-sky-300";
+			break;
+		case "Gatsby":
+			color = "bg-purple-300";
+			break;
+		case "Headless WordPress":
+			color = "bg-blue-300";
+			break;
+		case "Headless CMS":
+			color = "bg-emerald-300";
+			break;
+		case "Jamstack":
+			color = "bg-pink-500";
+			break;
+		case "GraphQL":
+			color = "bg-gray-300";
+			break;
+		case "SEO":
+			color = "bg-orange-300";
+			break;
+		case "Digital Marketing":
+			color = "bg-yellow-300";
+			break;
+		case "Blockchain":
+			color = "bg-gray-300";
+			break;
+		case "Bitcoin":
+			color = "bg-orange-300";
+			break;
+		case "NFTs":
+			color = "bg-green-300";
+			break;
+		default:
+			color = "bg-purple-300";
 	}
 
-	return edges.sort((a, b) => {
-		let titleA = a.node.title.toLowerCase();
-		let titleB = b.node.title.toLowerCase();
-		return titleA.localeCompare(titleB);
-	});
-}
+	return color;
+};
+
+export const getHeadingsFromMarkdown = (html) => {
+	// Load the HTML into cheerio
+	const $ = cheerio.load(html);
+
+	// Select all h2 and h3 elements
+	const headings = $("h2, h3")
+		.map((index, element) => {
+			const $el = $(element);
+			const isH2 = $el.is("h2");
+			const classes = isH2
+				? "text-md my-1 cursor-pointer font-garamond font-semibold text-gray-700 md:text-lg"
+				: "text-md my-1 cursor-pointer font-garamond font-semibold text-gray-600 md:text-md m-2";
+
+			return {
+				heading: $el.text(),
+				key: index,
+				classes,
+				slug: $el.attr("id"),
+			};
+		})
+		.get(); // Convert cheerio object to a plain array
+
+	return headings;
+};
