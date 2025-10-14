22fde3792974684d36928f81f8d5952b733d93cd
RedRocksWebDevelopment <websites@redrockswebdevelopment.com>
2025-10-14 01:15:50 -0600

    Test moving block functionality to MD

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,7 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
+import { file, glob } from "astro/loaders";
 import { useWordPressAPI } from "@lib/hooks";
-import { pageSchema, postSchema, categorySchema } from "./wordPressSchema";
+import { pageSchema } from "./wordPressSchema";
 
 export const pagesCollection = defineCollection({
 	schema: pageSchema,
@@ -13,30 +14,68 @@ export const pagesCollection = defineCollection({
 	},
 });
 
+export const authorsCollection = defineCollection({
+	loader: file("src/content/blog/authors.json"),
+	schema: ({ image }) =>
+		z.object({
+			id: z.string(),
+			slug: z.string(),
+			name: z.object({
+				first: z.string(),
+				last: z.string(),
+			}),
+			title: z.string().optional(),
+			email: z.string().optional(),
+			website: z.string().optional(),
+			social: z
+				.object({
+					x: z.string().optional(),
+					linkedin: z.string().optional(),
+					github: z.string().optional(),
+				})
+				.optional(),
+			avatar: z.object({
+				src: image(),
+				alt: z.string(),
+			}),
+			bio: z.string(),
+		}),
+});
+
 export const postsCollection = defineCollection({
-	schema: postSchema,
-	loader: async () => {
-		const posts = await useWordPressAPI.getAllPosts();
-		return posts.map((post) => ({
-			id: post.id,
-			...post,
-		}));
-	},
+	loader: glob({ pattern: "**/*.{md, mdx}", base: "./src/content/blog/posts" }),
+	schema: ({ image }) =>
+		z.object({
+			slug: z.string().regex(/^[a-z0-9-]+$/),
+			author: reference("authors"),
+			category: reference("categories"),
+			tags: z.array(reference("tags")).optional(),
+			title: z.string(),
+			date: z.coerce.date(),
+			seo: z.object({
+				title: z.string(),
+				description: z.string(),
+			}),
+			featuredImage: z.object({
+				src: image(),
+				alt: z.string(),
+			}),
+			excerpt: z.string(),
+		}),
 });
 
 export const categoriesCollection = defineCollection({
-	schema: categorySchema,
-	loader: async () => {
-		const categories = await useWordPressAPI.getAllCategories();
-		return categories.map((category) => ({
-			id: category.id,
-			...category,
-		}));
-	},
+	loader: file("src/content/blog/categories.json"),
+	schema: z.object({
+		slug: z.string(),
+		name: z.string(),
+		description: z.string(),
+	}),
 });
 
 export const collections = {
 	pages: pagesCollection,
 	posts: postsCollection,
+	authors: authorsCollection,
 	categories: categoriesCollection,
 };
