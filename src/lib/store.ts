615d275a9a477c92cfa4302e2ee8d29e7b782a1a
RedRocksWebDevelopment <websites@redrockswebdevelopment.com>
2025-10-13 21:22:59 -0600

    Add social link globals

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -4,6 +4,12 @@ export const isProduction = import.meta.env.PROD;
 
 export const siteUrl = import.meta.env.SITE;
 
+export const SOCIAL_LINKS = {
+    x: "https://x.com/intent/follow?screen_name=AndrewKepson",
+    linkedin: "https://www.linkedin.com/in/andrewkepson/",
+    github: "https://github.com/AndrewKepson",
+}
+
 const pagesCollection = await getCollection("pages");
 export const allWordPressPages = pagesCollection?.map((page) => page?.data);
 
