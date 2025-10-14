043f40fe68e42fcf9bbe34be3742f81b3037f404
RedRocksWebDevelopment <websites@redrockswebdevelopment.com>
2025-10-14 16:27:47 -0600

    Remove MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,21 +8,21 @@ import sitemap from "@astrojs/sitemap";
 import alpinejs from "@astrojs/alpinejs";
 
 export default defineConfig({
-    integrations: [react(), sitemap(), alpinejs()],
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    trailingSlash: "always",
-    site: "https://andrewkepson.com",
-    image: {
-        domains: ["headless.andrewkepson.com"],
-    },
-    prefetch: true,
-    compressHTML: true,
-    output: "static",
-    redirects: {
-        "/blog/create-headless-wordpress-source-gatsby-wordpress-api/":
-            "/blog/headless-wordpress/create-headless-wordpress-source-gatsby-wordpress-api/",
-        "/blog/how-and-why-nike-is-using-blockchain/": "/blog/blockchain/how-and-why-nike-is-using-blockchain/",
-    },
+	integrations: [react(), sitemap(), alpinejs()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	trailingSlash: "always",
+	site: "https://andrewkepson.com",
+	image: {
+		domains: ["headless.andrewkepson.com"],
+	},
+	prefetch: true,
+	compressHTML: true,
+	output: "static",
+	redirects: {
+		"/blog/create-headless-wordpress-source-gatsby-wordpress-api/":
+			"/blog/headless-wordpress/create-headless-wordpress-source-gatsby-wordpress-api/",
+		"/blog/how-and-why-nike-is-using-blockchain/": "/blog/blockchain/how-and-why-nike-is-using-blockchain/",
+	},
 });
